7f88cebeed9119a86d7d3a5edcadc8ee5fbee0f2
InomjonDev <inomjonismanaliev@gmail.com>
2025-12-02 19:17:17 +0500

    changes in category

--- src/validations/category.js
+++ src/validations/category.js
@@ -1,6 +1,7 @@
 const Joi = require('joi')
 
 const categorySchema = Joi.object({
+	userId: Joi.string().min(1).required(),
 	name: Joi.string().min(1).max(50).required(),
 	type: Joi.string().valid('income', 'expense').required(),
 	cat_id: Joi.string().min(1).required(),
